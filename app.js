bbee6ce6cb7009e7f116ed4e2b5f3bd4b406a83f
sbbaldwin <bal20020@byui.edu>
2024-04-02 11:05:48 -0600

    stuff

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 const cors = require('cors');
 
 
 const port = process.env.PORT || 8080;
 const app = express();
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 app
     .use(cors())
@@ -15,6 +20,39 @@ app
     .use('/', require('./routes/index'))
     .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://brichristiansenarrangements.onrender.com/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        // Here, you would find or create a user in your database
+        // and return that user instance by calling cb(null, user);
+        console.log(profile);
+        cb(null, profile);
+    }
+));
+
+// Serialize and deserialize user instances to and from the session.
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+// Use session management middleware
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful authentication, redirect to api-docs or another page.
+        res.redirect('/api-docs');
+    });
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
