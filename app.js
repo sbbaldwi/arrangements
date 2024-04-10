0391bc92f0f032748135ff89273657727750e5b2
sbbaldwin <bal20020@byui.edu>
2024-04-10 11:27:48 -0600

    clean up app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const connectDb = require('./db/connect');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const cors = require('cors');
 const path = require('path');
-const passport = require('passport');
+const indexRouter = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Setup session management
 app.use(session({
-    secret: 'secret',  // This should ideally be an environment variable
+    secret: 'secret',
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
@@ -21,49 +19,26 @@ app.use(session({
     })
 }));
 
-// Middleware to parse JSON and urlencoded data and set CORS
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Serve static files - should ideally be placed after dynamic route declarations
+app
+    .use(cors())
+    .use(express.json())
+    .use('/', indexRouter)
+    .use('/uploads', express.static('uploads'));
+
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-app.get('/accounts/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'login.html'));
-});
-
-app.get('/accounts/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'register.html'));
-});
-
-app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    function (req, res) {
-        // Successful authentication, redirect to /api-docs.
-        res.redirect('/api-docs');
-    });
-
-// Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Serve API routes
-app.use('/', require('./routes/index'));
-
-// Serve uploads as static files
-app.use('/uploads', express.static('uploads'));
-
-// Custom error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-});
-
-// Connection to MongoDB and server start
 connectDb().then(() => {
     app.listen(port, () => {
         console.log(`Connected to DB and listening on ${port}`);
     });
 }).catch(err => {
-    console.error('Database connection failed', err);
+    console.log('Database connection failed', err);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
 });
